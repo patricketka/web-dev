1c7993f2aa88cff152801fa79e2e98baf775a90e
Patrick Etka <patricketka@gmail.com>
2022-02-27 19:58:02 -0500

    a5 checkpoint push

---
+++ public/tuiter/ExploreScreen/ExploreComponent.js
@@ -0,0 +1,47 @@
+import PostSummaryList from "../PostSummaryList";
+
+const ExploreComponent = () => {
+    return(`
+           <div class="row mb-2">
+                <div class="input-group rounded-pill col">
+                    <input class="form-control rounded-pill" type="text" placeholder="&#x1F50E;Search Tuiter">
+                </div>
+                <div class="col-1 d-flex align-items-center">
+                    <a href="#">
+                        <i class="fa fs-3 fa-cog"></i>
+                    </a>
+                </div>
+           </div>
+           <div class="row mb-2">
+               <ul class="nav mb-2 nav-tabs">
+                    <li class="nav-item">
+                            <a class="nav-link active" href="for-you.html">For you</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="trending.html">Trending</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="news.html">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sports.html">Sports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link d-none d-md-grid" href="entertainment.html">Entertainment</a>
+                    </li>
+               </ul>
+           </div>
+           <div class="row">
+                <div class="card p-0">
+                <img src="../../images/spaceX.jpg" class="card-img">
+                <div class="card-img-overlay d-flex align-items-end p-1">
+                    <h3 class="text-white card-title mb-0">SpaceX's Starship</h3>
+                </div>
+           </div>
+           </div> 
+           <div class="row"
+                ${PostSummaryList()}
+           </div>               
+    `);
+}
+export default ExploreComponent;
---
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -0,0 +1,19 @@
+import NavigationSidebar from "../NavigationSidebar";
+import PostSummaryList from "../PostSummaryList";
+import PostList from "../PostList";
+
+(function ($) {
+    $('#wd-explore').append(`
+        <div class="row mt-2 mb-2">
+            <div class="col-2 col-md-2 col-lg-2 col-xl-2">
+                ${NavigationSidebar('Home')}
+            </div>
+            <div class="col-8 col-lg-6 col-xl-6">
+                ${PostList()}
+            </div>
+            <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                ${PostSummaryList()}
+            </div>
+        </div>
+    `);
+})($);
